602011c0470f62da97a6c9124e74a9115964f3cf
rafiqulislam <rafiqulislam98316844@gmail.com>
2021-09-13 12:46:48 -0700

    shop page coding done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <img src={logo} alt="" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">Review</a>
+                <a href="/manage">Manage Inventory</a>
+                <a href="/contact">Contact</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
